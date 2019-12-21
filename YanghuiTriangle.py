b92aeae2c7ef65da108249e419b68160ba00f14a
duhan9836 <57944666+duhan9836@users.noreply.github.com>
2019-12-21 09:21:25 +0000

    Update YanghuiTriangle.py
    add two different approaches of solving the yanghui problem. 1 and 3 have passed, with efficiency of O(n**3）and O(n**2）respectively, while 2 have not passed.

--- YanghuiTriangle.py
+++ YanghuiTriangle.py
@@ -16,3 +16,29 @@ def row(n):
         return y
 print(row(10))
 print(yanghui(10))
+
+
+
+def yanghui2(n):
+    if n == 1:
+        return [[1]]
+    if n == 2:
+        return [[1], [1, 1]]
+    if n>2:
+        y = [1]*n
+        for i in range(1, n-1):
+            y[i]=yanghui2(n-1)[-1][i-1]+yanghui2(n-1)[-1][i]
+        print(y)
+        return yanghui2(n-1).append(y)
+    
+    
+
+def yanghui3(n):
+    yanghui=[[1]*(i+1) for i in range(n)]
+    for i in range(2,n):
+        for j in range(1,i):
+            yanghui[i][j]=yanghui[i-1][j-1]+yanghui[i-1][j]
+    return yanghui
+
+print(yanghui3(10))
+
